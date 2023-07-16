38cd85895a015287dba7b7c9f5a69d47444cf873
Alexis mb <88553436+alexis-martinez-globant@users.noreply.github.com>
2023-07-16 19:31:11 -0300

    Security config

---
+++ backend-usersapp/src/main/java/com/mb/backend/usersapp/backendusersapp/auth/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.mb.backend.usersapp.backendusersapp.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+        return http.authorizeHttpRequests()
+        .requestMatchers(HttpMethod.GET, "/users").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .csrf(config -> config.disable())
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+        .build();
+
+}
+}
