1007f401c320eaf7ea20f15d8c9052ba9cb4ffe4
Alexis mb <88553436+alexis-martinez-globant@users.noreply.github.com>
2023-07-17 12:21:18 -0300

    updated

--- backend-usersapp/src/main/java/com/mb/backend/usersapp/backendusersapp/auth/SpringSecurityConfig.java
+++ backend-usersapp/src/main/java/com/mb/backend/usersapp/backendusersapp/auth/SpringSecurityConfig.java
@@ -1,24 +1,49 @@
 package com.mb.backend.usersapp.backendusersapp.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.mb.backend.usersapp.backendusersapp.auth.filters.JwtAuthenticationFilter;
 
 @Configuration
 public class SpringSecurityConfig {
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
-        return http.authorizeHttpRequests()
-        .requestMatchers(HttpMethod.GET, "/users").permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .csrf(config -> config.disable())
-        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
-        .build();
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
-}
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    // @Bean
+    // PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests()
+            .requestMatchers(HttpMethod.GET, "/users").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
+            .csrf(config -> config.disable())
+            .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+            .build();
+    }
 }
